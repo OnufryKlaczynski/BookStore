bba637f5675ba6ec0668b9b912c4762e800e3932
Onufry <onufryklaczynski@gmail.com>
2019-03-17 23:29:11 +0100

    Store\base.html header layout and style

---
+++ Store/static/Store/js/search.js
@@ -0,0 +1,14 @@
+
+
+function changeSearchFormColorOnFocus(){
+    let element = document.querySelector("form#search");
+    console.log(element);
+    element.setAttribute("style", "background-color: rgba(0, 0, 0, 0.06);")
+}
+
+function changeSearchFormColorOnBlur(){
+    let element = document.querySelector("form#search");
+    console.log(element);
+    element.setAttribute("style", "background-color: #fafafa;")
+}
+
